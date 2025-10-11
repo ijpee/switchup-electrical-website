1e3cc6d6658bcd5a8cc88c652b5a38b5aac60e87
Isaiah Poupouare <Isaiah.Poupouare@kindo.co.nz>
2025-10-11 21:02:01 +1300

    Switch to static build to fix deployment issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel({
-    includeFiles: [],
-    excludeFiles: [],
-  }),
-  vite: {
-    ssr: {
-      external: [],
-      noExternal: ['react', 'react-dom'],
-    },
-  },
+  output: "static",
 });
